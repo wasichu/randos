ea642b37756afa5f08e3fc577446bb8bb7e2dc32
Garrett McGrath <iamwasichu@gmail.com>
2026-05-24 13:47:32 -0500

    Less verbose logging in JS console

--- assets/js/randos/logging.js
+++ assets/js/randos/logging.js
@@ -1,10 +1,16 @@
 const LOG_PREFIX = "randos"
 
+const debugEnabled = () => window.localStorage.getItem("randos:debug") === "true"
+
 export const logEvent = (event, details = {}) => {
+  if (!debugEnabled()) return
+
   console.info(`[${LOG_PREFIX}]`, {event, ...details})
 }
 
 export const logDebug = (event, details = {}) => {
+  if (!debugEnabled()) return
+
   console.debug(`[${LOG_PREFIX}]`, {event, ...details})
 }
 
